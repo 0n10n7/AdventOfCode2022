c6fb2ab53b7b0aaa3356378b3e71bd2c7a695d46
mikael kökeritz <mikael.kokeritz@gmail.com>
2022-12-01 12:06:39 +0100

    Gold Day 1

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -22,20 +22,43 @@ namespace Day1
                 sum += currentNumber;
                 if(line == ""){
                     calories.Add(sum);
+                    Console.WriteLine("Sum is:");
                     Console.WriteLine(calories[counter]);
                     counter++;
                     sum=0;
                 }
+                // Console.WriteLine($"The current number is {currentNumber}");
+                // Console.WriteLine($"The current sum is {sum}");
+                // Console.WriteLine(line);
             }
             int maxCalorie = 0;
+            int secondMaxCalorie = 0;
+            int thirdMaxCalorie = 0;
             for(int i = 0; i< calories.Count; i ++){
                 int currentNumber = calories[i];
                 if(currentNumber > maxCalorie){
+                    thirdMaxCalorie = secondMaxCalorie;
+                    secondMaxCalorie = maxCalorie;
                     maxCalorie = currentNumber;
                 }
+                else if(currentNumber > secondMaxCalorie){
+                    thirdMaxCalorie = secondMaxCalorie;
+                    secondMaxCalorie = currentNumber;
+                }
+                else if(currentNumber > thirdMaxCalorie){
+                    thirdMaxCalorie = currentNumber;
+                }
+                Console.WriteLine(i);
             }
+            Console.WriteLine(calories.Count);
             Console.WriteLine("MAX CALORIE:");
             Console.WriteLine(maxCalorie);
+            Console.WriteLine("SECOND MAX CALORIE:");
+            Console.WriteLine(secondMaxCalorie);
+            Console.WriteLine("THIRD MAX CALORIE:");
+            Console.WriteLine(thirdMaxCalorie);
+            Console.WriteLine("SUM CALORIE:");
+            Console.WriteLine(maxCalorie + secondMaxCalorie + thirdMaxCalorie);
         }
     }
 }
