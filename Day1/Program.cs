2d15fca71d490f93f0155836fc1c442e63c5f892
mikael kökeritz <mikael.kokeritz@gmail.com>
2022-12-01 10:14:55 +0100

    Initilasation + Day1

---
+++ Day1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Program");
+            foreach (string line in System.IO.File.ReadLines(@"AdventOfCodeInputDay1.txt"))
+            {  
+                System.Console.WriteLine(line);
+            }  
+        }
+    }
+}
