1e72db803e52082076226016e56b4cde4496dcba
mikael kökeritz <mikael.kokeritz@gmail.com>
2022-12-03 08:07:37 +0100

    Day 4 Inialized

---
+++ Day4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            foreach (string line in System.IO.File.ReadLines(@"AdventOfCodeInputDay1.txt"))
+            {
+                
+            }
+            
+        }
+    }
+}
