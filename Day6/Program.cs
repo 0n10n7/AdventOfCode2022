504736240e61d7be9c7a49d0c85090ae2070cbd8
mikael kökeritz <mikael.kokeritz@gmail.com>
2022-12-06 09:46:11 +0100

    Day 6 Gold

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -7,43 +7,41 @@ namespace Day6
     {
         static void Main(string[] args)
         {
-            string fourLetters = "";
-            char firstLetter = ' ';
-            char secondLetter = ' ';
-            char thirdLetter = ' ';
-            char fourthLetter = ' ';
+            
+            List<char> Letters = new List<char>();
+            Letters.Add(' ');
             foreach (string line in System.IO.File.ReadLines(@"AdventOfCodeInputDay6.txt"))
             {
                 for(int i = 0; i < line.Length ; i++)
                 {
+                    string fourLetters = "";
                     Console.Write($"{i+1} :    ");
-                    fourthLetter = thirdLetter;
-                    Console.Write(fourthLetter);
-                    thirdLetter = secondLetter;
-                    Console.Write(thirdLetter);
-                    secondLetter = firstLetter;
-                    Console.Write(secondLetter);
-                    firstLetter = line[i];
-                    Console.Write($"{firstLetter} \t");
-                    fourLetters =  fourthLetter.ToString() + thirdLetter.ToString() +  secondLetter.ToString() + firstLetter.ToString();
+                    if(Letters.Count>=14)
+                    {
+                        Letters.RemoveAt(0);
+                    }
+
+                    Letters.Add(line[i]);
+                    for(int j = 0; j<Letters.Count;j++){
+                        fourLetters += Letters[j];
+                    }
+                    
                     int counter = 0;
-                    var duplicates = new List<char>();
 
                     Console.WriteLine(fourLetters);
                     foreach (var item in fourLetters)
                     {
-                        if (fourLetters.IndexOf(item) != fourLetters.LastIndexOf(item) && !duplicates.Contains(item))
+                        if (fourLetters.IndexOf(item) != fourLetters.LastIndexOf(item))
                         {
-                            duplicates.Add(item);
                         }
                         else{
                             counter++;
                         }
                     }
-                    if(counter == 4){
+                    if(counter == 14){
                         Console.WriteLine($" {i+1} :   WEVE GOT ONE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
                     }
-                    if(i+1 == 1000 || i+1 == 2000 || i+1 ==3000) Console.ReadLine();
+                    if(i +1 == 500 || i+1 == 1000 || i+1 == 2000 || i+1 ==3000) Console.ReadLine();
                     
                 }
                 
