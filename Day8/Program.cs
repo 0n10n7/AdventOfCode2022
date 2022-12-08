92824fdd27811cf07bc8af1858cb0771da73cf56
mikael kökeritz <mikael.kokeritz@gmail.com>
2022-12-08 06:46:29 +0100

    Bröd

---
+++ Day8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day8
+{
+    class Tree
+    {
+        int x;
+        int y;
+    }
+    class Program
+    {
+        int xPointer = 0;
+        int yPointer = 0;
+        List<Tree> trees= new List<Tree>();
+        static void Main(string[] args)
+        {
+            
+            foreach (string line in System.IO.File.ReadLines(@"AdventOfCodeInputDay8.txt"))
+            {
+                foreach (char Letter in line)
+            {
+                
+            }
+            }
+            
+        }
+    }
+}
